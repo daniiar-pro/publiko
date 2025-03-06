b6f8fc08b9a34ba97d585d7a28366c11a500c4dc
Daniiar Abdraiymov <abdraiymovd@gmail.com>
2025-03-06 04:54:46 +0100

    greet MVP

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,19 +1,41 @@
-import { AppRouter } from "../../server/src/controllers";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { AppRouter } from "../../server/src/controllers/index";
 
 //  Initialize our client
+let authToken: string | null = null;
 const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: "http://localhost:3000/api/v1/trpc",
-      headers: { Authorization: "TOKEN" },
+      headers: () => ({
+        Authorization: authToken ? `Bearer ${authToken}` : "",
+      }),
     }),
   ],
 });
 
+// async function logOutUser() {
+//   const logout = await client.user.logout.mutation();
+
+//   return logout;
+// }
+
 async function main() {
-  const test = await client.testIt.query();
-  console.log(test);
+  // const loginResponse = await client.user.login.mutation({
+  //   email: "user2@gmail.com",
+  //   password: "User21234",
+  // });
+
+  // authToken = loginResponse.token;
+  // const testIt = await client.user.testIt.query();
+
+  // console.log(testIt);
+
+  // const createPost = await client.post.create.mutation({
+  //   title: "Study in Poland",
+  //   content: "Good education",
+  // });
+  // console.log(`Post has been created successfully ${createPost}`);
 }
 
 main();
